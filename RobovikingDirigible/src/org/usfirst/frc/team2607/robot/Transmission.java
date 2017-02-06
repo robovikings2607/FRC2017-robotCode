789e6ba13d461ca7f544e22ca9ae980a7eb2a020
DavidRein <davidr1324@gmail.com>
2017-02-05 22:07:26 -0500

    fixed mishap with the Transmission file location

--- RobovikingDirigible/src/org/usfirst/frc/team2607/robot/Transmission.java
+++ RobovikingDirigible/src/org/usfirst/frc/team2607/robot/Transmission.java
@@ -1,49 +1,64 @@
 package org.usfirst.frc.team2607.robot;
 
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 
-public class Transmission implements SpeedController {
+public class Transmission implements SpeedController{
+	
+	CANTalon motor1 , motor2;
 
-	@Override
-	public void pidWrite(double output) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public double get() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+ public Encoder enc;
+ 
+public Transmission( int[]deviceID, int sourceA, int sourceB){
 
-	@Override
-	public void set(double speed) {
-		// TODO Auto-generated method stub
-		
-	}
+motor1 = new CANTalon(deviceID[1]);
+motor2 = new CANTalon(deviceID[2]);
+enc = new Encoder(sourceA, sourceB);
+}
 
-	@Override
-	public void setInverted(boolean isInverted) {
-		// TODO Auto-generated method stub
-		
-	}
+@Override
+public void pidWrite(double output) {
+	// TODO Auto-generated method stub
+	
+}
 
-	@Override
-	public boolean getInverted() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+@Override
+public double get() {
+	// TODO Auto-generated method stub
+	return motor1.get();
+}
 
-	@Override
-	public void disable() {
-		// TODO Auto-generated method stub
-		
-	}
+@Override
+public void set(double speed) {
+	// TODO Auto-generated method stub
+		 motor1.set (speed);
+	motor2.set(speed);
+}
 
-	@Override
-	public void stopMotor() {
-		// TODO Auto-generated method stub
-		
-	}
+@Override
+public void setInverted(boolean isInverted) {
+	// TODO Auto-generated method stub
+	
+}
 
+@Override
+public boolean getInverted() {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+@Override
+public void disable() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void stopMotor() {
+	// TODO Auto-generated method stub
+	
+}
 }
